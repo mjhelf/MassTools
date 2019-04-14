f7e2d59dba7392ff725c0a00708997321666d62a
mjhelf <mjh438@cornell.edu>
2019-04-13 22:54:44 -0400

    first performance overhaul, decreased overhead significantly for large molecules

--- R/Functions_MF_formatting.R
+++ R/Functions_MF_formatting.R
@@ -1,38 +1,46 @@
-#''reformatSF
+#''reformatMFstring
 #'
 #' Reformats a molecula formula string so that it has a count for each element, even if the count is 1.
 #' 
 #' @param s Molecula formula string
 #'
 #' @export
-reformatSF <- function(s){
+reformatMFstring <- function(s){
   
+  #finds adjacent element names
   foundLL <- regexpr("[A-z][A-Z]|[A-z]$",s)
   
-  if(foundLL == -1){
-    return(s)
-  }
-  else{
+  sel <- foundLL != -1
+  
+  while(any(sel)){
+   
+    s[sel] <- paste0(substr(s[sel],1,foundLL[sel]),"1", substr(s[sel],foundLL[sel]+1,nchar(s[sel])))
     
-    return( reformatSF(paste0(substr(s,1,foundLL),"1", substr(s,foundLL+1,nchar(s)))))
+    foundLL[sel] <- regexpr("[A-z][A-Z]|[A-z]$",s[sel])
+    
+    
+    sel <- foundLL != -1
+    
   }
+  
+  return(s)
   
 }
 
 
-#''consolidateSF
+#' consolidateMF
 #'
 #' Merges duplicates in an element count vector
 #' 
 #' @param x element count vector
 #'
 #' @export
-consolidateSF <- function(x){
+consolidateMF <- function(x){
   
   dups <- duplicated(names(x))
   
   if(!any(dups)){
-    class(counts) <- "MFobject"
+    class(x) <- "MFobject"
     return(x)
   }
   
@@ -53,30 +61,43 @@ consolidateSF <- function(x){
 }
 
 
-#''makeSF
+#' makeMF
 #'
 #' Reformat a molecula formula string into an element count vector
 #' 
 #' @param s Molecula formula string
+#' @param forcelist if \code{TRUE}, will always return a list, even when s is of length 1.
 #'
 #' @export
-makeSF <- function(s){
+makeMF <- function(s, forcelist = F){
   
-  s <- reformatSF(s)
-  elnums <- strsplit(s, "[A-Z]|[A-Z][a-z]")[[1]]
-  elnums <- as.integer(elnums[-1])
-  names(elnums) <- strsplit(s, "-[0-9]+|[0-9]+")[[1]]
-  return(consolidateSF(elnums))  
+  s <- reformatMFstring(s)
+  elnums <- strsplit(s, "[A-Z]|[A-Z][a-z]")
+  elnums <- lapply(elnums,function(el){as.integer(el[-1])})
+  elnames <- strsplit(s, "-[0-9]+|[0-9]+")
+  
+  elnums <- mapply(function(enu,ena){names(enu) <- ena
+  return(enu)}, enu = elnums, ena = elnames, SIMPLIFY = F)
+  
+  consolidated <- lapply(elnums,consolidateMF)
+  
+  if(!forcelist && length(consolidated) ==1){
+  return(consolidated[[1]])  
+  }else{
+    return(consolidated)  
+    
+  }
+    
 }
 
-#''remakeSF
+#''remakeMF
 #'
 #' Converts an element count vector back into a molecula formula string
 #' 
 #' @param s Molecula formula string
 #'
 #' @export
-remakeSF <- function(x){
+remakeMF <- function(x){
   
   return(paste(names(x[x!=0]), x[x!=0], collapse = "", sep = ""))
   
