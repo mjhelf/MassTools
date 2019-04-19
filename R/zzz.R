ec1ddb944f0ba8a86e2d2a563f1ad49e277bdcc4
mjhelf <mjh438@cornell.edu>
2019-04-19 00:53:55 -0400

    cleanup and addition of some elements

--- R/zzz.R
+++ R/zzz.R
@@ -4,17 +4,19 @@
   
   
   elementNames <- c("C","H","N","O","P","S", 
-                               "F", "Cl", "Br", "I", "Si", "B", "As",
+                               "F", "Cl", "Br", "I", "Si",
+                               "B", "As", "Se", 
                                "Li", "Na", "K", "Mg", "Ca",
                                "Fe", "Co", "Mn", "Cu", "Zn")
   
   maxValences <-c("C" = 4,
                   "H" = 1,
-                  "N" = 3,
+                  "N" = 4,
                   "O" = 2,
                   "P" = 5,
                   "S" = 6, 
-                  "F" = 1, "Cl" = 1, "Br" = 1, "I" = 1, "Si" = 4, "B" = 3, "As" = 5,
+                  "F" = 1, "Cl" = 1, "Br" = 1, "I" = 1, "Si" = 4,
+                  "B" = 3, "As" = 5, "Se" = 6,
                   "Li" = 1, "Na" = 1, "K" = 1, "Mg" = 2, "Ca" = 2,
                   "Fe" = 3, "Co" = 4, "Mn" = 4, "Cu" = 2, "Zn" =2)
   
